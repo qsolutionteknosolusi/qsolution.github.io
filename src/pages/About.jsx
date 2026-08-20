d5bfb9a545cbb422d98815bfc1fb1d2b953d23a0
qsolution tekno solusi PT <qsolutionteknosolusipt@gmail.com>
2026-08-20 22:13:50 +0700

    Setup React Router

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-blue-700">
+        About Page
+      </h1>
+    </div>
+  );
+}
