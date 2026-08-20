fbf3b869077d90324519a5bfbdaf545fd0fe33f2
qsolution tekno solusi PT <qsolutionteknosolusipt@gmail.com>
2026-08-20 23:16:02 +0700

    Fix About import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 
 function App() {
   return (
